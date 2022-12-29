344d97387a711e0210b2708503585f07b234bc1d
Alexis Galvez <cpnalexisgalvez@gmail.com>
2022-12-28 23:30:57 -0300

    tercera-preentrega-js

---
+++ js/stock.js
@@ -0,0 +1,49 @@
+const productos = [
+   {
+        id: 1,
+        nombre: "Producto 1",        
+        precio: 1000000,        
+        img: './img/producto1.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Producto 2",        
+        precio: 1000000,        
+        img: './img/producto2.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Producto 3",        
+        precio: 1000000,        
+        img: './img/producto3.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Producto 4",        
+        precio: 1000000,        
+        img: './img/producto4.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Producto 5",        
+        precio: 1000000,        
+        img: './img/producto5.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Producto 6",        
+        precio: 1000000,        
+        img: './img/producto6.jpg',
+        cantidad:1
+    }
+]
